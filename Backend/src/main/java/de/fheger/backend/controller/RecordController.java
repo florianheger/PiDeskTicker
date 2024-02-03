6443924431e6d682024e95e0762900b5bb10a853
Florian Heger <florian.heger@outlook.de>
2024-02-03 22:05:36 +0100

    Added ugly solution for get latest

--- Backend/src/main/java/de/fheger/backend/controller/RecordController.java
+++ Backend/src/main/java/de/fheger/backend/controller/RecordController.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/record")
@@ -15,8 +18,17 @@ public class RecordController {
 
     @GetMapping("/latest")
     public ResponseEntity<Record> getLatestMeasuringPoint() {
-        return ResponseEntity.ok(new Record());
-        //return ResponseEntity.ok(recordRepository.findTopByOrderByIdDesc());
+        //return ResponseEntity.ok(new Record());
+        // return ResponseEntity.ok(recordRepository.findTopByOrderByIdDesc());
+        List<Record> records = recordRepository.findAll();
+        AtomicReference<Record> record = new AtomicReference<>(new Record());
+        record.get().setId(-1);
+        records.forEach(r -> {
+            if (record.get().getId() < r.getId()) {
+                record.set(r);
+            }
+        });
+        return ResponseEntity.ok(record.get());
     }
 
     @PostMapping
--- Backend/src/main/java/de/fheger/backend/model/Record.java
+++ Backend/src/main/java/de/fheger/backend/model/Record.java
@@ -1,14 +1,19 @@
 package de.fheger.backend.model;
 
+import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "record")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Record {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -18,5 +23,6 @@ public class Record {
 
     float humidity;
 
+    @CreationTimestamp
     LocalDateTime dateTime;
 }
